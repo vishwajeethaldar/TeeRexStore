d51626c92bad9ff5be98dcbb8ea3a8893fefe002
vishwajeethaldar <onlyjeet3@gmail.com>
2023-05-26 17:07:37 +0530

    test32

---
+++ src/components/test/test.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { errorContext } from '../../context/ErrorContext'
+import styles from '../styles/AlertMsg.module.css';
+
+// Alert Component this component is to diplay alert messages on screen
+export default function AlertMsg({type="success", description, title, duration}:{type:string, description:string, title:string,duration:number}) {
+  const {error, setError} = React.useContext(errorContext)
+
+  React.useEffect(()=>{
+      if(error){
+        setTimeout(()=>{
+          setError(false)
+        }, duration*1000)
+      }
+  },[error])
+  return (
+    error&&
+    <div className={styles.container}>
+      <div className={styles[type]}>
+          <div className={styles.titleContainer}>
+            <h4 className={styles.alerttitle}> {title} </h4> 
+            <h4 className={styles.closealert} onClick={()=>setError(false)}> X </h4>
+          </div>
+          <p className={styles.alertdescription}> {description}</p>
+    </div>
+    </div>
+  )
+}
